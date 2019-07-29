35498a3aa75c0f650f212aaf4d6eb05413931f47
eunsoossi <eunsoossi@gmail.com>
2019-07-29 16:22:33 +0900

    Character Animation
    - Character animation fixing needed
    - Animation with blendShapes(for face) recording needed

---
+++ AIsPrivacyPrj/Assets/ChattingInterupt/Scripts/LookControll.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookControll : MonoBehaviour
+{
+    private Animator anim;
+    public LookFollow lookFollow;
+    bool eyeFollow = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        lookFollow = GetComponent<LookFollow>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !anim.GetBool("look"))
+        {
+            anim.SetBool("look", true);
+            if (anim.GetBool("look"))
+            {
+                lookFollow.lookOn = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && anim.GetBool("look"))
+        {
+            anim.SetBool("look", false);
+            if (!anim.GetBool("look"))
+            {
+                lookFollow.lookOn = false;
+            }
+        }
+    }
+}
---
+++ AIsPrivacyPrj/Assets/ChattingInterupt/Scripts/LookFollow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookFollow : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public bool lookOn;
+    public float speed;
+
+    Animator anim;
+    Transform head;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        head = anim.GetBoneTransform(HumanBodyBones.Head);
+        lookOn = false;
+    }
+
+    void LateUpdate()
+    {
+        if (lookOn)
+        {
+            Looking();
+        }
+    }
+
+
+    public void Looking()
+    {
+        // head.LookAt(target.position);
+        // head.rotation = head.rotation * Quaternion.Euler(offset);
+        Quaternion lookRotation = Quaternion.LookRotation(target.position - head.position);
+        // Debug.Log(target.position + "dfsdfds" + lookRotation);
+        head.rotation = Quaternion.Lerp(head.rotation, lookRotation, Time.deltaTime * speed);
+        // Debug.Log(head.rotation);
+        // 애니메이션을 켜고 하면 안됨. 왜일까? 애니메이션 켜고 래이트 업데이트로 하면 또 살짝 됨.
+
+    }
+}
