74607e7cc15173bad1174c2fec38f20393f038dd
eunsoossi <eunsoossi@gmail.com>
2019-10-31 09:50:45 +0900

    longtime no see

---
+++ AIsPrivacyPrj/Assets/ChattingInterupt/Scripts/FaceRecorder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor.Animations;
+#endif
+
+public class FaceRecorder : MonoBehaviour
+{
+
+    public AnimationClip clip;
+    private GameObjectRecorder m_recoeder;
+    public bool record = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_recoeder = new GameObjectRecorder(gameObject);
+        
+        m_recoeder.BindComponentsOfType<SkinnedMeshRenderer>(gameObject, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clip == null)
+        {
+            return;
+        }
+
+        if (record)
+        {
+            m_recoeder.TakeSnapshot(Time.deltaTime);
+        }
+
+        else if (m_recoeder.isRecording)
+        {
+            m_recoeder.SaveToClip(clip);
+            m_recoeder.ResetRecording();
+        }
+    }
+}
